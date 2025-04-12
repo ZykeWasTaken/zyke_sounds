1377a0255053fb5aedf9944e55251dcc32e0aca8
ZykeWasTaken <linussnilsson03@gmail.com>
2025-04-13 01:58:25 +0200

    fix(ensure maxDistance decimal)

--- client/debug.lua
+++ client/debug.lua
@@ -2,12 +2,15 @@ if (not Config.Settings.debug) then return end
 
 CreateThread(function()
     while (true) do
-        for soundId, soundData in pairs(Cache.activeSounds) do
+        for _, soundData in pairs(Cache.activeSounds) do
             if (soundData.pos) then
-                DrawMarker(28, soundData.pos.x, soundData.pos.y, soundData.pos.z, 0, 0, 0, 0, 0, 0, 0.25, 0.25, 0.25, 255, 255, 255, 200, false, false, 2, nil, nil, false)
+                -- Sound position
+                ---@diagnostic disable-next-line: param-type-mismatch
+                DrawMarker(28, soundData.pos.x, soundData.pos.y, soundData.pos.z, 0, 0, 0, 0, 0, 0, 0.25, 0.25, 0.25, 255, 255, 255, 200, false, false, 2, nil, nil, false, false)
 
                 -- Max distance
-                DrawMarker(28, soundData.pos.x, soundData.pos.y, soundData.pos.z, 0, 0, 0, 0, 0, 0, soundData.maxDistance, soundData.maxDistance, soundData.maxDistance, 255, 255, 255, 100, false, false, 2, nil, nil, false)
+                ---@diagnostic disable-next-line: param-type-mismatch
+                DrawMarker(28, soundData.pos.x, soundData.pos.y, soundData.pos.z, 0, 0, 0, 0, 0, 0, soundData.maxDistance + 1.0, soundData.maxDistance + 1.0, soundData.maxDistance + 1.0, 255, 255, 255, 100, false, false, 2, nil, nil, false, false)
             end
         end
 
