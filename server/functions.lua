3a3cdefb30abe001b0d7ceb7e9f288933df8b92f
ZykeWasTaken <linussnilsson03@gmail.com>
2025-04-13 04:00:11 +0200

    feat(moved functions to functions.lua)

---
+++ server/functions.lua
@@ -0,0 +1,68 @@
+---@param soundName string
+function PlaySound(soundName)
+    TriggerClientEvent("zyke_sounds:PlaySound", -1, soundName)
+end
+
+local soundIdCounter = 0
+
+---@param entity integer
+---@param id string? @Only needed if you want to manually stop the sound
+---@param soundName string
+---@param maxVolume number
+---@param maxDistance number
+---@param playCount integer? @If not looping, you can decide how many times the audio will play
+---@return string
+function PlaySoundOnEntity(entity, id, soundName, maxVolume, maxDistance, looped, playCount)
+    local entityNetId = NetworkGetNetworkIdFromEntity(entity)
+    soundIdCounter += 1
+
+    ---@type SoundDataWithEntity
+    local soundData = {
+        soundId = id or ("sound-" .. soundIdCounter),
+        soundName = soundName,
+        entityNetId = entityNetId,
+        soundType = "entity",
+        maxVolume = maxVolume or 0.3,
+        maxDistance = maxDistance or 10.0,
+        looped = looped or false,
+        playCount = playCount or 1
+    }
+
+    TriggerClientEvent("zyke_sounds:PlaySound", -1, soundData)
+
+    return soundData.soundId
+end
+
+exports("PlaySoundOnEntity", PlaySoundOnEntity)
+
+---@param location vector3
+---@param soundName string
+---@return string
+function PlaySoundOnLocation(location, soundName)
+    soundIdCounter += 1
+
+    ---@type SoundDataWithLocation
+    local soundData = {
+        soundId = "sound-" .. soundIdCounter,
+        soundName = soundName,
+        location = location,
+        soundType = "location",
+        maxVolume = 0.3,
+        maxDistance = 10.0,
+        looped = false
+    }
+
+    TriggerClientEvent("zyke_sounds:PlaySound", -1, soundData)
+
+    return soundData.soundId
+end
+
+exports("PlaySoundOnLocation", PlaySoundOnLocation)
+
+---@param soundId string
+---@param fade? number
+function StopSound(soundId, fade)
+    TriggerClientEvent("zyke_sounds:StopSound", -1, soundId, fade)
+end
+
+exports("StopSound", StopSound)
